59b93275b6000e73acff665d9de942e834005956
Patrik Piskay <piskay.patrik@gmail.com>
2016-01-09 22:26:34 +0100

    license, release script

---
+++ scripts/release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+if ! [ -e scripts/release.sh ]; then
+  echo >&2 "Please run scripts/release.sh from the repo root"
+  exit 1
+fi
+
+update_version() {
+  echo "$(node -p "p=require('./${1}');p.version='${2}';JSON.stringify(p,null,2)")" > $1
+  echo "Updated ${1} version to ${2}"
+}
+
+current_version=$(node -p "require('./package').version")
+
+printf "Next version (current is $current_version)? "
+read next_version
+
+next_ref="v$next_version"
+
+npm test -- --single-run
+
+update_version 'package.json' $next_version
+
+git commit --allow-empty -am "Version $next_version"
+
+git tag $next_ref
+
+git push origin master
+git push origin $next_ref
+
+npm run clean
+npm run build
+npm run build:dist
+
+npm publish
